51725adc737bc076350830b71d25664a7c951519
yoursvinayy <112862282+yoursvinayy@users.noreply.github.com>
2025-04-17 15:27:11 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   swcMinify: true,
   experimental: {
-    forceSwcTransforms: true
-  }
-}
+    forceSwcTransforms: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname, "app"), // Yeh line ADD karni hai
+    };
+    return config;
+  },
+};
 
-module.exports = nextConfig 
+module.exports = nextConfig;
